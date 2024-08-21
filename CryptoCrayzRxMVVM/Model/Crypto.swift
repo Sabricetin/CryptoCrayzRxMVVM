6fd82e97e5be94bfc890dd7ac6416175bedb7ad1
Sabricetin <sabriyazilim@gmail.com>
2024-08-21 22:07:54 +0300

    first commit

---
+++ CryptoCrayzRxMVVM/Model/Crypto.swift
@@ -0,0 +1,14 @@
+//
+//  Crypto.swift
+//  CryptoCrayzRxMVVM
+//
+//  Created by Sabri Çetin on 21.08.2024.
+//
+
+import Foundation
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
+
---
+++ CryptoCrayzRxMVVM/View/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CryptoCrayzRxMVVM
+//
+//  Created by Sabri Çetin on 21.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cryptoList = [Crypto]()
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+      
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cryptoList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = cryptoList[indexPath.row].currency
+        content.secondaryText = cryptoList[indexPath.row].price
+        cell.contentConfiguration = content
+        return cell
+        
+    }
+}
+
