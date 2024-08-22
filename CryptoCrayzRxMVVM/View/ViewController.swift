c152f3946c0d1d0d155d3e226b25ca22f6f2574c
Sabricetin <sabriyazilim@gmail.com>
2024-08-22 15:31:58 +0300

    Update Version

--- CryptoCrayzRxMVVM/View/ViewController.swift
+++ CryptoCrayzRxMVVM/View/ViewController.swift
@@ -6,22 +6,66 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource{
+class ViewController: UIViewController , UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    
+    let cryptoVM = CryptoViewModel()
+    let disposeBag = DisposeBag()
     
     var cryptoList = [Crypto]()
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        tableView.delegate = self
-        tableView.dataSource = self
+       // tableView.delegate = self
         
-      
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        
+        setupBindings()
+        cryptoVM.requestData()
+        
+    }
+    private func  setupBindings() {
+        
+        cryptoVM
+            .loading
+            .bind(to: self.indicatorView.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        cryptoVM
+            .error
+            .subscribe(on: MainScheduler.asyncInstance)
+            .subscribe { errorString in
+                print (errorString)
+            }
+            .disposed(by: disposeBag)
+        
+       /* cryptoVM
+            .cryptos
+            .subscribe(on: MainScheduler.asyncInstance)
+            .subscribe { cryptos in
+                self.cryptoList  = cryptos
+                self.tableView.reloadData()
+            }
+            .disposed(by: disposeBag)
+        */
+        
+      cryptoVM
+            .cryptos
+            .bind(to: tableView.rx.items(cellIdentifier: "CryptoCell", cellType: CryptoTableViewCell.self)) {row,item,cell in
+                cell.item = item
+            }
+            .disposed(by: disposeBag)
     }
     
+    
+    /*
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cryptoList.count
     }
@@ -35,6 +79,7 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
         cell.contentConfiguration = content
         return cell
         
-    }
+    } */
+    
 }
 
